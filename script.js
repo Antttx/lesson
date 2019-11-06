fc81be4bb99a1bf45f522d8132920d388476fa0c
Antoxa <ant.oxa@bk.ru>
2019-11-07 00:10:39 +0500

    lesson 4 not done

--- script.js
+++ script.js
@@ -16,47 +16,46 @@ let obj = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: false
-};
-
-function chooseExpenses(){
-	for (let i = 0; i < 2; i++) {
-		let a = prompt("Введите обязательную статью расходов в этом месяце", "");
-			b = prompt("Во сколько обойдется?", "");
-
-		if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 && b.length < 50 ) {
-			obj.expenses[a] = b;
+	savings: false,
+	chooseExpenses: function(){
+		for (let i = 0; i < 2; i++) {
+			let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+				b = prompt("Во сколько обойдется?", "");
+	
+			if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 && b.length < 50 ) {
+				obj.expenses[a] = b;
+			} else {
+				console.log('Ещё раз');
+				i--;
+			}
+		}
+	},
+	detectDayBudget: function(){
+		obj.moneyPerDay = obj.budget / 30;
+		console.log("Ежедневный бюджет: " + obj.moneyPerDay.toFixed(2));
+	},
+	detectLevel: function(){
+		if (obj.moneyPerDay < 100){
+			console.log("Минимальный уровень достатка");
+		} else if (obj.moneyPerDay > 100 && obj.moneyPerDay < 1000){
+			console.log("Средний уровень достатка");
+		} else if (obj.moneyPerDay > 1000){
+			console.log("Хороший уровень достатка");
 		} else {
-			console.log('Ещё раз');
-			i--;
+			console.log("Ошибка данных");
+		}
+	},
+	checkSavings: function(){
+		let save = +prompt("Какова сумма накопления?"),
+		percent = +prompt('Под какой процент?');
+		obj.mathIncome = save / 100 /12 *percent;
+		alert("Доход в месяц в вашева депозита: " + obj.mathIncome.toFixed(2));
+	},
+	chooseOptExpenses: function(){
+		obj.savings = confirm('Вы имеете накопление?');
+		if (obj.savings == true){
+			checkSavings();
 		}
 	}
-}
+};
 
-chooseExpenses();
-
-obj.moneyPerDay = obj.budget / 30;
-console.log("Ежедневный бюджет: " + obj.moneyPerDay.toFixed(2));
-
-if (obj.moneyPerDay < 100){
-	console.log("Минимальный уровень достатка");
-} else if (obj.moneyPerDay > 100 && obj.moneyPerDay < 1000){
-	console.log("Средний уровень достатка");
-} else if (obj.moneyPerDay > 1000){
-	console.log("Хороший уровень достатка");
-} else {
-	console.log("Ошибка данных");
-}
-
-function checkSavings(){
-	let save = +prompt("Какова сумма накопления?"),
-		percent = +prompt('Под какой процент?');
-
-	obj.mathIncome = save / 100 /12 *percent;
-	alert("Доход в месяц в вашева депозита: " + obj.mathIncome.toFixed(2));
-}
-
-obj.savings = confirm('Вы имеете накопление?');
-if (obj.savings == true){
-	checkSavings();
-}
